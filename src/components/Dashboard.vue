136440a56536e9075008599858055fe4e40b78e4
Madi <reds1n1gam1@gmail.com>
2025-09-09 02:01:35 +0300

    Dashboard draft version

---
+++ src/components/Dashboard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-for="(item, index) in list" :key="index">
+        {{  item.name }}
+    </div>
+</template>
+
+<script setup lang="ts">
+type CheckupItem = {
+    name: string;
+}
+
+const list: CheckupItem[] = [
+    {
+        name: 'test1'
+    }, {
+        name: 'test2'
+    },
+    {
+        name: 'test3'
+    },
+]
+</script>
+<style scoped></style>
